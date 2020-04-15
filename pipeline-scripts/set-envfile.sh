cd2b6cf6ebc8898a3f37fde24bc8b5cc4e499a8c
Flavian Iordache <flavian.iordache@allcloud.io>
2020-04-15 17:53:28 +0300

    3flv

--- pipeline-scripts/set-envfile.sh
+++ pipeline-scripts/set-envfile.sh
@@ -2,4 +2,3 @@
 aws s3 cp s3://prod-exampal-source-bucket/script-envfile-optimized-nosecrets.sh /tmp --region us-east-1
 chmod +x /tmp/script-envfile-optimized-nosecrets.sh && sh /tmp/script-envfile-optimized-nosecrets.sh
 mv /tmp/envfile.env /home/raful/default/app/.env
-echo "successfully set the env file" > /home/ubuntu/output.txt
